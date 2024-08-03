b14177237fcbd7213ee887c5883683853152c076
gaurav703 <111629507+gaurav703@users.noreply.github.com>
2024-08-03 21:07:34 +0530

    img

--- src/containers/Skills/Skills.jsx
+++ src/containers/Skills/Skills.jsx
@@ -34,17 +34,56 @@ const Skills = () => {
       ],
     },
   ];
-  const [skills, setSkills] = useState([]);
-  useEffect(() => {
-    const query = '*[_type== "experiences"]';
-    const skillsQuery = '*[_type== "skills"]';
-    // client.fetch(query).then((data) => {
-    //   setExperiences(data);
-    // });
-    client.fetch(skillsQuery).then((data) => {
-      setSkills(data);
-    });
-  }, []);
+  const [skills, setSkills] = useState([
+    {
+      name: "HTML",
+      bgColor: "#f16529",
+    },
+    {
+      name: "CSS",
+      bgColor: "#2965f1",
+    },
+    {
+      name: "JavaScript",
+      bgColor: "#f0db4f",
+    },
+    {
+      name: "React.js",
+      bgColor: "#61dbfb",
+    },
+    {
+      name: "Node.js",
+      bgColor: "#68a063",
+    },
+    {
+      name: "Express.js",
+      bgColor: "#000",
+    },
+    {
+      name: "MongoDB",
+      bgColor: "#4db33d",
+    },
+    {
+      name: "Git",
+      bgColor: "#f34f29",
+    },
+    {
+      name: "Tailwind CSS",
+      bgColor: "#06b6d4",
+    },
+    {
+      name: "Next.js",
+      bgColor: "#000",
+    },
+    {
+      name: "React Native",
+      bgColor: "#61dbfb",
+    },
+    {
+      name: "Figma",
+      bgColor: "#f24e1e",
+    },
+  ]);
   const { mode } = useTheme();
   return (
     <>
@@ -52,18 +91,18 @@ const Skills = () => {
 
       <div className="app__skills-container">
         <motion.div className="app__skills-list">
-          {skills?.map((skill) => (
+          {skills?.map((skill, index) => (
             <motion.div
               whileInView={{ opacity: [0, 1] }}
               transition={{ duration: 0.5 }}
               className={`app__skills-item ${mode} app__flex`}
-              key={skill.name}
+              key={index}
             >
               <div
                 className="app__flex"
-                style={{ backgroundColor: skill.bgColor }}
+                style={{ backgroundColor: "rgb(237, 242, 248)" }}
               >
-                <img src={urlFor(skill.icon)} alt={skill.name} />
+                <img src="/git.png" alt="git " />
               </div>
               <p className={`p-text ${mode}`}>{skill.name}</p>
             </motion.div>
